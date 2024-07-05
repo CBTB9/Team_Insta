cd6c2d6188e98d585a17c1b5d6cc14fd761f7db8
MalkuSR <m.s.malik3105@gmail.com>
2024-07-05 19:43:26 +0500

    Create insta.js

---
+++ js/insta.js
@@ -0,0 +1,20 @@
+ // Example of how you might toggle active class based on clicked item
+// document.querySelectorAll('.sidebar a').forEach(item => {
+//     item.addEventListener('click', () => {
+//         document.querySelectorAll('.sidebar a').forEach(el => el.classList.remove('active'));
+//         item.classList.add('active');
+//     });
+// });
+
+
+// ====== like btn ======
+function likebutton(){
+    let heart = document.querySelector(".heart");
+    if (heart.src.match("assets/svg/heart.svg")){
+        heart.src = "assets/svg/heart_red.svg";
+    }
+    else{
+        heart.src = "assets/svg/heart.svg"
+    }
+}
+// ======================
